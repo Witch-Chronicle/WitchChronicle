156868de3d9da6c9bd4c6643f3f639b1e19e55f1
Ku-Dong-Hyeon <144973004+Ku-Dong-Hyeon@users.noreply.github.com>
2026-07-30 10:50:51 +0900

    던전 이벤트 수정

--- Assets/_Scripts/Dungeon/Room/EventDataSo.cs
+++ Assets/_Scripts/Dungeon/Room/EventDataSo.cs
@@ -3,6 +3,18 @@ using UnityEngine;
 
 public enum Event_Type { Reward, Trap, Debuff }
 
+/// <summary>이벤트가 파티에 주는 효과 종류.</summary>
+public enum EventEffectKind
+{
+    None,           // 효과 없음(메시지만)
+    HealHp,         // HP 회복 (Value = 고정 수치)
+    HealHpPercent,  // HP 회복 (Value = 최대 HP 대비 %)
+    HealMp,         // MP 회복 (Value = 고정 수치)
+    HealMpPercent,  // MP 회복 (Value = 최대 MP 대비 %)
+    DamageHp,       // HP 피해 (Value = 고정 수치)
+    DamageHpPercent // HP 피해 (Value = 최대 HP 대비 %)
+}
+
 [CreateAssetMenu(fileName = "NewEventData", menuName = "Dungeon/Event Data")]
 public class EventDataSO : ScriptableObject
 {
@@ -13,6 +25,8 @@ public class EventDataSO : ScriptableObject
     public Event_Type Type;
 
     [Header("Effect Settings")]
+    [Tooltip("파티에 적용할 효과 종류. None이면 메시지만 표시")]
+    public EventEffectKind EffectKind = EventEffectKind.None;
     public int Value; // 데미지량, 회복량, 혹은 아이템 ID 등
     public GameObject EffectPrefab; // 발동 시 보여줄 파티클 등
 
